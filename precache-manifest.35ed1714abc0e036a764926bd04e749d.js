eaac0e86274932c1eb9b5ed3a3f906536fa57314
ThatDevCoder <bordia999@gmail.com>
2020-04-10 02:07:36 +0530

    Updates

---
+++ precache-manifest.35ed1714abc0e036a764926bd04e749d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/udacity-react-projectOne/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/udacity-react-projectOne/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/udacity-react-projectOne/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "75449c63760dd22378f1",
+    "url": "/udacity-react-projectOne/static/js/runtime~main.75449c63.js"
+  },
+  {
+    "revision": "d28b84d6755415573ba8",
+    "url": "/udacity-react-projectOne/static/js/main.d28b84d6.chunk.js"
+  },
+  {
+    "revision": "8e5338491ab3d661dc13",
+    "url": "/udacity-react-projectOne/static/js/1.8e533849.chunk.js"
+  },
+  {
+    "revision": "d28b84d6755415573ba8",
+    "url": "/udacity-react-projectOne/static/css/main.4cc650bb.chunk.css"
+  },
+  {
+    "revision": "fe34ac362e3a61dea089c5be7e2e0580",
+    "url": "/udacity-react-projectOne/index.html"
+  }
+];
